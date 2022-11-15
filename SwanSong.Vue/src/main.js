a738f4684494e973b7b47694e7bee34920f18e82
John Miller <john.miller@unionstreet.co.uk>
2022-11-15 11:28:24 +0000

    remove element plus theme

--- SwanSong.Vue/src/main.js
+++ SwanSong.Vue/src/main.js
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from './plugins/font-awesome'
 import ElementPlus from 'element-plus';
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'element-plus/lib/theme-chalk/index.css'; 
 import './assets/site.css'; 
 import { Search } from '@element-plus/icons'  
 import './utils/prototypes/DateToShortFormat' 
